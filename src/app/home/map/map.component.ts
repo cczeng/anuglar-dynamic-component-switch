f3de8632d8bbfd9ae6926105ad868bb9216921e1
cczeng <zengliangi@163.com>
2018-04-26 17:15:58 +0800

    加入更多动态创建组件的操作

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -31,6 +31,7 @@ export class MapComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     if (this.cmpRef) {
+      this.target.clear();
       this.cmpRef.destroy();
     }
   }
@@ -40,6 +41,18 @@ export class MapComponent implements OnInit, OnDestroy {
 
     const factory = this.componentFactoryResolver.resolveComponentFactory(map);   //生成一个component
     this.cmpRef = this.target.createComponent(factory)     // 将生成的component创建到容器视图中
+
+    // 可通过 this.componentRef.instance 接口调用创建的组件
+    // this.componentRef.instance.data = this._myData;
+
+    // 也可以直接修改 @input
+    // this.componentRef.instance.initData = this._initData;
+    // @input() initData;
+
+    // 监听动态创建的组件事件
+    // this.componentRef.instance.createZone.subscribe(zone => console.log(zone));
+    // 动态创建的组件
+    // @output() createZone = new EventEmitter();
 
     // this.cmpRef.instance   这个对象可以直接调用动态创建出来的组件
     this.cdRef.detectChanges();
