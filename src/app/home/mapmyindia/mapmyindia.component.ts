df1cde4c5d860abbb55f106d2053f3b05281b54d
cczeng <zengliangi@163.com>
2018-04-26 15:32:44 +0800

    添加印度地图

--- src/app/home/mapmyindia/mapmyindia.component.ts
+++ src/app/home/mapmyindia/mapmyindia.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapmyindia.component.less']
 })
 export class MapmyindiaComponent implements OnInit {
+  map;
 
   constructor() { }
 
   ngOnInit() {
+    this.map = new MapmyIndia.Map("mapmyindia", {
+      center: [28.61, 77.23], zoomControl: true, hybrid: true
+    });
+  }
+
+  flyToPoint(point) {
+    this.map.flyTo([point[1], point[0]], 10);
   }
 
 }
