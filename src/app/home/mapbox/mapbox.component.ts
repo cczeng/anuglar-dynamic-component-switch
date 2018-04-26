6a5c743769d9b835c2822c0439810e764c9ae2b0
cczeng <zengliangi@163.com>
2018-04-26 15:02:22 +0800

    add mapbox

--- src/app/home/mapbox/mapbox.component.ts
+++ src/app/home/mapbox/mapbox.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as mapboxgl from 'mapbox-gl';
+import * as MapboxDraw from 'mapbox-gl-draw';
+
 @Component({
   selector: 'app-mapbox',
   templateUrl: './mapbox.component.html',
@@ -10,6 +13,11 @@ export class MapboxComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiaXJ2ZWVzIiwiYSI6ImNqYmRhZ3FmNjF4NGgyenFnMzJsdnRuOTUifQ.2JZUeZxUr_ckr3NNJlbePg';
+    const map = new mapboxgl.Map({
+      container: 'mapbox',
+      style: 'mapbox://styles/mapbox/streets-v9'
+    });
   }
 
 }
